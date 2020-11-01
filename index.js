d5be7defdd20bd05c1785e09f3986e557fdc4923
Avudayappan Palanivel <sibia53@gmail.com>
2020-11-01 11:38:21 +0530

    pipeline

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function (request, response){
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World from expressjs in a Docker container. Avudayappan Palanivel.");
+  response.end("Hello World from expressjs in a Docker container. Avudayappan .");
 });
 
 server.listen(8000);
